7987c85fe6f4fb30bb239a59b6669bf5fbdd28d8
Ecks <ecks@Eckss-MBP.attlocal.net>
2023-07-03 16:45:03 -0400

    user updated to server

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,29 @@ dotenv.config();
 const dbConnect = require("./config/db/dbConnect");
 
 const app = express();
+
 // database
 dbConnect();
+
+// register
+app.post("/api/users/register", (req, res) => {
+    res.json({ user: "User Registered" });
+});
+
+// login
+app.post("/api/users/login", (req, res) => {
+    res.json({ user: "User Login" });
+});
+
+// fetch all user
+app.get("/api/users", (req, res) => {
+    res.json({ user: "fetch all user" });
+});
 
 // console .env
 console.log(process.env);
 
 // server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, console.log(`Server is running ${PORT}`));
