1d751cff4f92f28ebf4afc3055aa1a83e5ba99e7
Ecks <ecks@Eckss-MBP.attlocal.net>
2023-07-04 10:04:57 -0400

    fetch all updated, will need for front end

--- backend/controllers/users/usersCtrl.js
+++ backend/controllers/users/usersCtrl.js
@@ -35,6 +35,7 @@ const loginUserCtrl = expressAsyncHandler(async (req, res) => {
 
     if (userFound && (await userFound.isPasswordMatched(password))) {
         res.json({
+            _id: userFound?._id,
             firstName: userFound?.firstName,
             lastName: userFound?.lastName,
             email: userFound?.email,
@@ -48,5 +49,15 @@ const loginUserCtrl = expressAsyncHandler(async (req, res) => {
     }
 });
 
+// fetch users
+const fetchUsersCtrl = expressAsyncHandler(async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.json(users);
+    } catch (error) {
+        res.json(error);
+    }
+});
 
-module.exports = { userRegisterCtrl, loginUserCtrl };
+
+module.exports = { userRegisterCtrl, loginUserCtrl, fetchUsersCtrl };
--- backend/route/users/usersRoute.js
+++ backend/route/users/usersRoute.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { userRegisterCtrl, loginUserCtrl, } = require('../../controllers/users/usersCtrl');
+const { userRegisterCtrl, loginUserCtrl, fetchUsersCtrl } = require('../../controllers/users/usersCtrl');
 
 const userRoutes =  express.Router();
 
 userRoutes.post('/register', userRegisterCtrl);
 userRoutes.post('/login', loginUserCtrl);
-
+userRoutes.get('/', fetchUsersCtrl);
 module.exports = userRoutes; 
