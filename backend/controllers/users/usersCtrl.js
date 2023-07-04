1b52b6edf3cd21ac400cc5e8e1b077fbbbe28bbd
Ecks <ecks@Eckss-MBP.attlocal.net>
2023-07-03 21:06:30 -0400

    seed database test connections

---
+++ backend/controllers/users/usersCtrl.js
@@ -0,0 +1,22 @@
+const User = require("../../model/user/User");
+
+// register 
+
+
+const userRegisterCtrl = async (req, res) => {
+    console.log(req.body);
+    try {
+    // register user // will create what you want to create from the model
+        const user = await User.create({
+            firstName: req?.body?.firstName, //short hand for firstName
+            lastName: req?.body?.lastName, //short hand for lastName
+            email: req?.body?.email, //short hand for email
+            password: req?.body?.password, //short hand for password
+        });
+        res.json(user);
+    } catch (error) {
+        res.json(error);
+    }
+};
+
+module.exports = { userRegisterCtrl };
