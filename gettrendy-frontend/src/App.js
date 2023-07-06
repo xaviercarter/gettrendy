c841471c9bbd9b29073c84ef42746e03aa325dfa
Ecks <ecks@Eckss-MBP.attlocal.net>
2023-07-06 19:06:20 -0400

    deleted app and recreated it due to tailwind css not working properly bug error is fixed now

--- gettrendy-frontend/src/App.js
+++ gettrendy-frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1 className="text-yellow-400">Gettrendy Fashion Blog</h1>
+      <h1 className="text-yellow-400">Hello world!</h1>
     </div>
   );
 }
